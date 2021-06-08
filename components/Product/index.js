c3569bd5f4625445c175e3bc00e0b892faa8657f
giorgos_gr <giorgos@webdimension.gr>
2021-06-08 18:49:37 +0300

    work with product page

--- components/Product/index.js
+++ components/Product/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from './product.module.scss';
 import { Col, Container, Row } from "react-bootstrap";
 
 const ProductOne = () => {
@@ -21,7 +22,54 @@ const ProductOne = () => {
       <div className="row">
         <div class="col-lx-1 d-sm-none ">small Images</div>
         <div class="col-5 ">largs Images</div>
-        <div class="col-6 ">contect</div>
+
+        <div class="col-6 description">
+            
+            <img src='./assets/images/product-image.png' alt='' />
+            
+            <p className={classes.title}>
+                L' Oreal Professionnel Pro Longer
+                <br />
+                Shampoo (300ml)
+            </p>
+
+            <p className={classes.code}>Κωδ: <span>LRL-E277300</span></p>
+
+            <p className={classes.availability}>
+                Διαθεσιμότητα Άμεση
+                <span>
+                   &nbsp; (1 έως 2 εργάσιμες)
+                </span>
+            </p>
+
+            <div className='d-flex p-0'>
+                <div>
+                    <p className={classes.price}>13,02 €</p>
+                    <p className={classes.priceWin}>Κερδίζετε 5.58€</p>
+                </div>
+                <div>
+                    <del className={classes.priceDel}>18,60 €</del>
+                </div>
+            </div>
+
+            <p className='mt-3'>Σαμπουάν Κατά της Ψαλίδας που αποτελεί την ιδανική περοποίηση για μαλλία με λέπτυνση και τάση για ψαλίδα στις άκρες.</p>
+
+            <div className={classes.choice}>
+                <span style={{fontWeight:'bold', marginRight: '1rem'}}>Απόχρωση</span>
+                <select>
+                    <option value="volvo">Επιλέξτε</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+            </div>
+
+            <div className={classes.details}>
+                <span style={{fontWeight:'bold'}}>Αποκλειστικά στο Zizel Beauty Shop: &nbsp;</span>
+                Το συγκεκριμένο προϊόν συνοδεύεται από θήκη προστασίας & μεταφοράς αξίας 29,90€ !
+            </div>
+
+        </div>
       </div>
     </Container>
   );
