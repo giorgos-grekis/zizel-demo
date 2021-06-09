9d4cf5cd1aaa87a3cf486dd48a22210a047c93ec
giorgos_gr <giorgos@webdimension.gr>
2021-06-09 13:47:16 +0300

    work with product

--- components/Product/index.js
+++ components/Product/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import classes from './product.module.scss';
-import Link from 'next/link'
+import classes from "./product.module.scss";
+import Link from "next/link";
 
-import ButtonsPruducts from './buttonsPruducts';
+import ButtonsPruducts from "./buttonsPruducts";
 import { AiOutlineHeart } from "react-icons/ai";
+import {SiFacebook} from 'react-icons/si'
+import {AiFillTwitterCircle} from 'react-icons/ai'
+import {FaPinterest} from 'react-icons/fa'
 import { Col, Container, Row } from "react-bootstrap";
 
 const ProductOne = () => {
@@ -22,82 +25,138 @@ const ProductOne = () => {
     //   </Row>
     // </Container>
 
-    <Container className="myContainer">
+    <Container className="myContainer my-5">
       <div className="row">
-        <div class="col-lx-1 d-sm-none ">small Images</div>
-        <div class="col-5 ">largs Images</div>
+
+          {/* product thumbnail */}
+        <div class="col-sm-1">
+          <div className={`${classes.productThumbnail}`}>
+            <img
+              src="./assets/images/product-1.jpg"
+              alt="product-1"
+            //   style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+
+        {/* product images */}
+        <div class="col-5 ">
+          <div className={classes.productImage}>
+            <img
+              src="./assets/images/product-1.jpg"
+              alt="product-1"
+            //   style={{ width: "100%" }}
+            />
+            <div className={classes.discount}>
+                -20%
+            </div>
+
+            <div className={classes.new}>
+                NEW
+            </div>
+          </div>
+          {/* social icons */}
+          {/* <div className='row mx-auto w-50 mt-3'>
+                <div 
+                className={`${classes.socialShare} col-3 mr-auto`} 
+                >share</div>
+
+                <div className={`${classes.social} col-2 mr-auto`}>
+                <SiFacebook />
+                </div>
+                <div className={`${classes.social} col-2 mr-auto`}>
+                    <AiFillTwitterCircle />
+                </div>
+                <div className={`${classes.social} col-2 mr-auto`}>
+                    <FaPinterest />
+                </div>
+             </div> */}
+
+             <div className={classes.socialAll}>
+                    <span className={`${classes.socialShare} `}>share</span>
+                    <SiFacebook className={`${classes.social}`}/>
+                    <AiFillTwitterCircle className={`${classes.social}`}/>
+                    <FaPinterest className={`${classes.social}`}/>
+             </div>
+        </div>
 
         <div class="col-6 description">
-            
-            <img src='./assets/images/product-image.png' alt='' />
-            
-            <p className={classes.title}>
-                L' Oreal Professionnel Pro Longer
-                <br />
-                Shampoo (300ml)
-            </p>
+          <img src="./assets/images/product-image.png" alt="" />
 
-            <p className={classes.code}>Κωδ: <span>LRL-E277300</span></p>
+          <p className={classes.title}>
+            L' Oreal Professionnel Pro Longer
+            <br />
+            Shampoo (300ml)
+          </p>
 
-            <p className={classes.availability}>
-                Διαθεσιμότητα Άμεση
-                <span>
-                   &nbsp; (1 έως 2 εργάσιμες)
-                </span>
-            </p>
+          <p className={classes.code}>
+            Κωδ: <span>LRL-E277300</span>
+          </p>
 
-            <div className='d-flex p-0'>
-                <div>
-                    <p className={classes.price}>13,02 €</p>
-                    <p className={classes.priceWin}>Κερδίζετε 5.58€</p>
-                </div>
-                <div>
-                    <del className={classes.priceDel}>18,60 €</del>
-                </div>
+          <p className={classes.availability}>
+            Διαθεσιμότητα Άμεση
+            <span>&nbsp; (1 έως 2 εργάσιμες)</span>
+          </p>
+
+          <div className="d-flex p-0">
+            <div>
+              <p className={classes.price}>13,02 €</p>
+              <p className={classes.priceWin}>Κερδίζετε 5.58€</p>
             </div>
-
-            <p className='mt-3'>Σαμπουάν Κατά της Ψαλίδας που αποτελεί την ιδανική περοποίηση για μαλλία με λέπτυνση και τάση για ψαλίδα στις άκρες.</p>
-
-            <div className={classes.choice}>
-                <span style={{fontWeight:'bold', marginRight: '1rem'}}>Απόχρωση</span>
-                <select>
-                    <option value="volvo">Επιλέξτε</option>
-                    <option value="saab">Απόχρωση 1</option>
-                    <option value="mercedes">Απόχρωση 2</option>
-                    <option value="audi">Απόχρωση 3</option>
-                </select>
+            <div>
+              <del className={classes.priceDel}>18,60 €</del>
             </div>
+          </div>
 
-            <div className={classes.details}>
-                <span style={{fontWeight:'bold'}}>Αποκλειστικά στο Zizel Beauty Shop: &nbsp;</span>
-                Το συγκεκριμένο προϊόν συνοδεύεται από θήκη προστασίας & μεταφοράς αξίας 29,90€ !
-            </div>
+          <p className="mt-3">
+            Σαμπουάν Κατά της Ψαλίδας που αποτελεί την ιδανική περοποίηση για
+            μαλλία με λέπτυνση και τάση για ψαλίδα στις άκρες.
+          </p>
 
-            <div className={classes.underline}></div>
+          <div className={classes.choice}>
+            <span style={{ fontWeight: "bold", marginRight: "1rem" }}>
+              Απόχρωση
+            </span>
+            <select>
+              <option value="volvo">Επιλέξτε</option>
+              <option value="saab">Απόχρωση 1</option>
+              <option value="mercedes">Απόχρωση 2</option>
+              <option value="audi">Απόχρωση 3</option>
+            </select>
+          </div>
 
-            <div className='row'>
-                <div className='col-3'>
-                    <ButtonsPruducts />
-                    {/* <div className="row mx-auto">
+          <div className={classes.details}>
+            <span style={{ fontWeight: "bold" }}>
+              Αποκλειστικά στο Zizel Beauty Shop: &nbsp;
+            </span>
+            Το συγκεκριμένο προϊόν συνοδεύεται από θήκη προστασίας & μεταφοράς
+            αξίας 29,90€ !
+          </div>
+
+          <div className={classes.underline}></div>
+
+          <div className="row">
+            <div className="col-3">
+              <ButtonsPruducts />
+              {/* <div className="row mx-auto">
                         <div className="col-3">-</div>
                         <div className="col-3">1</div>
                         <div className="col-3">+</div>
                     </div> */}
-                </div>
-                <div className='col-7'>
-                    <Link href={'/cart'}>
-                        <a className={classes.button}>
-                            ΠΡΟΣΘΗΚΗ ΣΤΟ ΚΑΛΑΘΙ
-                        </a>
-                    </Link>
-                </div>
-                <div className='col-2'>
-                 <AiOutlineHeart className={classes.icons}/>
-                </div>
             </div>
-
+            <div className="col-7">
+              <Link href={"/cart"}>
+                <a className={classes.button}>ΠΡΟΣΘΗΚΗ ΣΤΟ ΚΑΛΑΘΙ</a>
+              </Link>
+            </div>
+            <div className="col-2">
+              <AiOutlineHeart className={classes.icons} />
+            </div>
+          </div>
         </div>
       </div>
+
+      <div className={classes.underline}></div>
     </Container>
   );
 };
